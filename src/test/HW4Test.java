2ea3410d839c9d5da962d2e2c03bf6c291657949
Zelendewagen <Zelendewagen@gmail.com>
2023-10-06 05:02:10 +1000

    HW4

---
+++ src/test/HW4Test.java
@@ -0,0 +1,47 @@
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import HW4.book.Book;
+import HW4.book.BookRepository;
+import HW4.book.BookService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class HW4Test {
+
+    private BookService bookService;
+    private BookRepository bookRepository;
+
+    @BeforeEach
+    void setUp() {
+        bookRepository = mock(BookRepository.class);
+        bookService = new BookService(bookRepository);
+    }
+
+    @Test
+    void testFindBookById() {
+        String bookId = "1";
+        Book expectedBook = new Book(bookId, "Book 1", "Author 1");
+        when(bookRepository.findById(bookId)).thenReturn(expectedBook);
+
+        Book result = bookService.findBookById(bookId);
+
+        assertEquals(expectedBook, result);
+    }
+
+    @Test
+    void testFindAllBooks() {
+        List<Book> expectedBooks = new ArrayList<>();
+        expectedBooks.add(new Book("1", "Book 1", "Author 1"));
+        expectedBooks.add(new Book("2", "Book 2", "Author 2"));
+
+        when(bookRepository.findAll()).thenReturn(expectedBooks);
+        List<Book> result = bookService.findAllBooks();
+
+        assertEquals(expectedBooks, result);
+    }
+}
